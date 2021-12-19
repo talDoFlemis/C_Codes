3da40ea5c63880ea7815e7f298180edcf00681db
Flemis <coderflemis@gmail.com>
2021-12-19 10:25:54 -0300

    adicionado a persistencia no disco e uso de cache, mas por enquanto só funciona se usar .exit pra fechar o arquivo

--- sqlite/main.c
+++ sqlite/main.c
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 typedef struct{
     char* buffer;
@@ -57,9 +60,15 @@ const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
+typedef struct{
+    int file_descriptor;
+    uint32_t file_lenght;
+    void* pages[TABLE_MAX_PAGES];
+}Pager;
+
 typedef struct {
-  uint32_t num_rows;
-  void* pages[TABLE_MAX_PAGES];
+    uint32_t num_rows;
+    Pager* pager;
 } Table;
 
 typedef enum { 
@@ -97,8 +106,101 @@ void print_prompt(void){
     printf("db > ");
 }
 
-MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+Pager* pager_open (const char* filename){
+    int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
+
+    if(fd==-1){
+        printf("Não foi possível abrir o arquivo");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t file_lenght = lseek(fd, 0, SEEK_END);
+    Pager* pager = malloc(sizeof(Pager));
+    pager->file_descriptor = fd;
+    pager->file_lenght = file_lenght;
+
+    for (int i = 0; i<TABLE_MAX_PAGES; i++){
+        pager->pages[i] = NULL;
+    }
+    return pager;
+}
+
+Table* db_open(const char* filename){
+    Pager* pager =  pager_open(filename);
+    uint32_t num_rows = pager->file_lenght / ROW_SIZE;
+
+    Table* table = malloc(sizeof(Table));
+    table->pager = pager;
+    table->num_rows = num_rows;
+    // for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+    //     table->pages[i] = NULL;
+    // }
+    return table;
+}
+
+void* pager_flush(Pager* pager, uint32_t page_num, uint32_t size ){
+    if(pager->pages[page_num] == NULL){
+        printf("Tentando flushar uma página nula\n");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+
+    if(offset == -1){
+        printf("Erro ao procurar %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+    ssize_t bytes_writen = write(pager->file_descriptor, pager->pages[page_num], size);
+
+    if(bytes_writen == -1){
+        printf("Erro ao tentar escrever no db%d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void db_close(Table* table){
+    Pager* pager = table->pager;
+    uint32_t num_full_pages = table->num_rows/ROWS_PER_PAGE;
+
+    for (uint32_t i = 0; i < num_full_pages; i++)
+    {
+        if(pager->pages[i] == NULL){
+            continue;
+        }
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
+    }
+    
+    uint32_t num_aditional_pages = table->num_rows%ROWS_PER_PAGE;
+    if(num_aditional_pages>0){
+        uint32_t page_num = num_full_pages;
+        if(pager->pages[page_num]!=NULL){
+            pager_flush(pager, page_num, num_aditional_pages*ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] == NULL;
+        }
+    }
+
+    int result = close(pager->file_descriptor);
+    if(result == -1){
+        printf("Erro ao tentar fechar o banco de dados\n");
+        exit(EXIT_FAILURE);
+    }
+    for (uint32_t i = 0; i<TABLE_MAX_PAGES; i++){
+        void* page = pager->pages[i];
+        if(page){
+            free(page);
+            pager->pages[i] = NULL;
+        }
+    }
+    free(pager);
+    free(table);
+}
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table){
     if(strcmp(input_buffer->buffer, ".exit")==0){
+        db_close(table);
         exit(EXIT_SUCCESS);
     }else{
         return META_COMMAND_UNRECOGNIZED;
@@ -106,47 +208,54 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 }
 
 void print_row(Row* row) {
-  printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
 void serialize_row(Row* source, void* destination) {
-  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    strncpy(destination + USERNAME_OFFSET,source->username, USERNAME_SIZE);
+    strncpy(destination + EMAIL_OFFSET, source->email, EMAIL_SIZE);
+
 }
 
 void deserialize_row(void* source, Row* destination) {
-  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
-  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
-  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void* get_page(Pager* pager, uint32_t page_num){
+    if(page_num >TABLE_MAX_PAGES){
+        printf("Tentando pegar o número da pagína fora dos limites. %d > %d\n", page_num, TABLE_MAX_PAGES);
+        exit(EXIT_FAILURE);
+    }
+    if(pager->pages[page_num] == NULL){
+        void* page = malloc(sizeof(PAGE_SIZE));
+        uint32_t num_pages = pager->file_lenght/PAGE_SIZE;
+        
+        if(pager->file_lenght%PAGE_SIZE){
+            num_pages += 1;
+        }
+        if(page_num<=num_pages){
+            lseek(pager->file_descriptor, page_num*PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+
+            if(bytes_read==-1){
+                printf("Erro ao tentar ler o arquivo %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+        }
+        pager->pages[page_num] = page;
+    }
+    return pager->pages[page_num];
 }
 
 void* row_slot(Table* table, uint32_t row_num) {
-  uint32_t page_num = row_num / ROWS_PER_PAGE;
-  void* page = table->pages[page_num];
-  if (page == NULL) {
-    // Allocate memory only when we try to access page
-    page = table->pages[page_num] = malloc(PAGE_SIZE);
-  }
-  uint32_t row_offset = row_num % ROWS_PER_PAGE;
-  uint32_t byte_offset = row_offset * ROW_SIZE;
-  return page + byte_offset;
-}
-
-Table* new_table() {
-  Table* table = malloc(sizeof(Table));
-  table->num_rows = 0;
-  for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-     table->pages[i] = NULL;
-  }
-  return table;
-}
-
-void free_table(Table* table) {
-  for (int i = 0; table->pages[i]; i++) {
-     free(table->pages[i]);
-  }
-  free(table);
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = get_page(table->pager, page_num);
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
 }
 
 ExecuteResult execute_insert(Statement* statement, Table* table) {
@@ -206,12 +315,12 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 }
 
 ExecuteResult execute_select(Statement* statement, Table* table) {
-  Row row;
-  for (uint32_t i = 0; i < table->num_rows; i++) {
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; i++) {
     deserialize_row(row_slot(table, i), &row);
     print_row(&row);
-  }
-  return EXECUTE_SUCCESS;
+    }
+    return EXECUTE_SUCCESS;
 }
 
 
@@ -226,7 +335,14 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
 }
 
 int main (int argc, char* argv[]){
-    Table* table = new_table();
+    if(argc<2){
+        printf("Por favor, você deve fornecer um arquivo\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char* filename = argv[1];
+    Table* table = db_open(filename);
+
     InputBuffer* input_buffer = new_input_buffer();
 
     while(true){
@@ -234,7 +350,7 @@ int main (int argc, char* argv[]){
         read_input(input_buffer);
 
         if(input_buffer->buffer[0] == '.'){
-            switch (do_meta_command(input_buffer))
+            switch (do_meta_command(input_buffer, table))
             {
             case (META_SUCCESS):
                 continue;
